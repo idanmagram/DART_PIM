84e582008c9e3078c134b1c769b18381902dba78
idanmagram <idan.magram@gmail.com>
2024-02-17 12:08:59 +0200

    Added WF, switched vector<char> to string in kmer, added calculateReadLocation

--- minmizerCreator.cpp
+++ minmizerCreator.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_set>
+
 
 using namespace std;
 
@@ -14,20 +16,26 @@ using namespace std;
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
 
 class Kmer {
 public:
     int position;
-    vector<int> kmerSeq;
+    string kmerSeq;
 
-    Kmer(int pos, const vector<int> &kmerSeq) : position(pos), kmerSeq(kmerSeq) {}
+    Kmer(int pos, const string &kmerSeq) : position(pos), kmerSeq(kmerSeq) {}
+};
 
+class RowMinimizer {
+public:
+    Kmer min;
+    string refSeq;
+
+    RowMinimizer(Kmer min, string refSeq) : min(min), refSeq(refSeq) {}
 };
 
 
-bool isLexBefore(const vector<int> &seq1, const vector<int> &seq2);
 bool compareMinimizers(const Kmer* a, const Kmer* b);
-
 
 class Heap {
 public:
@@ -39,11 +47,11 @@ public:
         size_t left = 2 * index + 1;
         size_t right = 2 * index + 2;
 
-        if (left < size && isLexBefore(elements[left]->kmerSeq, elements[largest]->kmerSeq)) {
+        if (left < size && (elements[left]->kmerSeq < elements[largest]->kmerSeq)) {
             largest = left;
         }
 
-        if (right < size && isLexBefore(elements[right]->kmerSeq, elements[largest]->kmerSeq)) {
+        if (right < size && (elements[right]->kmerSeq < elements[largest]->kmerSeq)) {
             largest = right;
         }
 
@@ -64,7 +72,7 @@ public:
 
         while (index > 0) {
             size_t parent = (index - 1) / 2;
-            if (!isLexBefore(elements[index]->kmerSeq, elements[parent]->kmerSeq)) {
+            if (elements[index]->kmerSeq > elements[parent]->kmerSeq) {
                 break;
             }
 
@@ -79,23 +87,21 @@ public:
     }
 
     Kmer* minElement() {
-        return (elements[0]);
+        return elements[0];
     }
-
 };
 
-
-vector<Kmer*> createMinimizers(const vector<int> &seq) {
+std::vector<Kmer*> createMinimizers(const string &seq) {
     int i;
-    vector<Kmer*> heapKmers;
-    vector<Kmer*> kmersInOrder;
+    std::vector<Kmer*> heapKmers;
+    std::vector<Kmer*> kmersInOrder;
     Kmer* minMinimizer;
-    vector<Kmer*> outMinimizers;
+    std::vector<Kmer*> outMinimizers;
 
     for(i = 0; (i < seq.size()) && (i < W); i++) {
-        std::vector<int> subVector(seq.begin() + i, seq.begin() + i + K);
+        std::string subString = seq.substr(i, K);
 
-        minMinimizer = new Kmer(i, subVector);
+        minMinimizer = new Kmer(i, subString);
         heapKmers.push_back(minMinimizer);
         kmersInOrder.push_back(minMinimizer);
     }
@@ -104,47 +110,99 @@ vector<Kmer*> createMinimizers(const vector<int> &seq) {
     for(; (i <= seq.size() - K + 1); i++) {
         auto currentMin = heapMin.minElement();
         if (outMinimizers.empty()) {
-            auto minElement = reinterpret_cast<Kmer *const>(heapMin.minElement());
-            outMinimizers.push_back(new Kmer(minElement->position, minElement->kmerSeq));
+            outMinimizers.push_back(new Kmer(currentMin->position, currentMin->kmerSeq));
         }
         else if(currentMin->position != outMinimizers.back()->position) {
-                outMinimizers.push_back(reinterpret_cast<Kmer *const>(heapMin.minElement()));
+            outMinimizers.push_back(new Kmer(currentMin->position, currentMin->kmerSeq));
         }
-        vector<int> subVector(seq.begin() + i, seq.begin() + i + K);
-        auto newMinimizer = new Kmer(i, subVector);
+        std::string subString = seq.substr(i, K);
+        auto newMinimizer = new Kmer(i, subString);
         kmersInOrder.push_back(newMinimizer);
         auto elem1 = kmersInOrder[kmersInOrder.size() - W - 1];
         auto iteratorToDelete = std::find(heapMin.elements.begin(), heapMin.elements.end(), elem1);
         heapMin.elements.erase(iteratorToDelete);
 
-        minMinimizer = reinterpret_cast<Kmer*>(heapMin.minElement());
         heapMin.insert(newMinimizer);
-
-        if(isLexBefore(newMinimizer->kmerSeq, minMinimizer->kmerSeq)) {
-            minMinimizer = newMinimizer;
-        }
     }
     return outMinimizers;
 }
 
-bool isLexBefore(const vector<int> &seq1, const vector<int> &seq2) {
-    for (int i = 0; i < seq1.size(); i++) {
-        if(seq1[i] < seq2[i]) {
-            return true;
-        }
-        else if (seq1[i] > seq2[i]) {
-            return false;
-        }
-    }
-    return false;
+bool compareMinimizers(const Kmer* a, const Kmer* b) {
+    return b->kmerSeq < a->kmerSeq;
 }
 
-bool compareMinimizers(const Kmer* a, const Kmer* b) {
-    return !isLexBefore(a->kmerSeq, b->kmerSeq);
+int wagnerFischerAffineGap(const string& S1, const string& S2, int wop, int wex, int wsub) {
+    int n = S1.size();
+    int m = S2.size();
+
+    // Initialize matrices D, M1, and M2 with appropriate dimensions
+    vector<vector<int>> D(n + 1, vector<int>(m + 1, 0));
+    vector<vector<int>> M1(n + 1, vector<int>(m + 1, 0));
+    vector<vector<int>> M2(n + 1, vector<int>(m + 1, 0));
+
+    // Initialize matrices with appropriate values
+    for (int i = 1; i <= n; ++i) {
+        D[i][0] = i * wex;
+        M1[i][0] = i * wex;
+    }
+    for (int j = 1; j <= m; ++j) {
+        D[0][j] = j * wex;
+        M2[0][j] = j * wex;
+    }
+
+    // Fill in the matrices using dynamic programming
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            M1[i][j] = min(M1[i - 1][j] + wex, D[i - 1][j] + wop + wex);
+            M2[i][j] = min(M2[i][j - 1] + wex, D[i][j - 1] + wop + wex);
+
+            if (S1[i - 1] == S2[j - 1]) {
+                D[i][j] = D[i - 1][j - 1];
+            } else {
+                D[i][j] = min({M1[i][j], M2[i][j], D[i - 1][j - 1] + wsub});
+            }
+        }
+    }
+    // Return the optimal alignment score
+    return D[n][m];
+}
+
+void calculateReadLocation(string read,  vector<RowMinimizer*> &MinimizersListForCPU) {
+    vector<Kmer*> minimizers = createMinimizers(read);
+    int EdScore = -1;
+    int minEdScore = MinimizersListForCPU[0]->refSeq.size();
+    int address = 0;
+    map<Kmer*,int> mapKmerToAdressInReq;
+
+    for (Kmer* minimizer : minimizers) {
+        for (const auto& row : MinimizersListForCPU) {
+            if (row->min.kmerSeq == minimizer->kmerSeq) {
+                EdScore = wagnerFischerAffineGap(read, row->refSeq, 1, 1, 1);
+                if(EdScore < minEdScore) {
+                    address = row->min.position;
+                    minEdScore = EdScore;
+                }
+            }
+        }
+        if (EdScore != -1) {
+            mapKmerToAdressInReq[minimizer] = address;
+        }
+        EdScore = -1;
+    }
 }
 
 int main() {
-    createMinimizers({2,3,1,0,3,2,1,0,1,2,3,3,1,0,1});
+    createMinimizers("231032101233101");
+    wagnerFischerAffineGap("231", "231", 1, 1, 1);
+    vector<RowMinimizer*> MinimizersListForCPU;
+    Kmer min1 = Kmer(0,"123");
+    Kmer min2 = Kmer(6,"231");
+    Kmer min3 = Kmer(8,"023");
+    MinimizersListForCPU.push_back(new RowMinimizer(min1, "0231032221233101"));
+    MinimizersListForCPU.push_back(new RowMinimizer(min2, "222032101233101"));
+    MinimizersListForCPU.push_back(new RowMinimizer(min3, "231032101233221"));
+
+    calculateReadLocation("231032101233101", MinimizersListForCPU);
+
     return 0;
 }
-
