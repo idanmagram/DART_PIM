2b42433de55348324a339a385a4f5ac459e476a2
idanmagram <idan.magram@gmail.com>
2024-03-22 10:51:52 +0200

    WF timing check

--- minmizerCreator.cpp
+++ minmizerCreator.cpp
@@ -10,8 +10,9 @@ using namespace std;
 #define C 1
 #define G 2
 #define T 3
-#define K 3
+#define K 12
 #define W 4
+#define N 7
 
 #include <iostream>
 #include <vector>
@@ -36,6 +37,8 @@ public:
 
 
 bool compareMinimizers(const Kmer* a, const Kmer* b);
+int wagnerFischerAffineGap2(const string& S1, const string& S2, int* score,  bool backtraching, int wop, int wex, int wsub);
+
 
 class Heap {
 public:
@@ -79,6 +82,7 @@ public:
             std::swap(elements[index], elements[parent]);
             index = parent;
         }
+        heapify(0);
     }
 
     void eraseLastMin() {
@@ -161,48 +165,208 @@ int wagnerFischerAffineGap(const string& S1, const string& S2, int wop, int wex,
             } else {
                 D[i][j] = min({M1[i][j], M2[i][j], D[i - 1][j - 1] + wsub});
             }
+
         }
     }
     // Return the optimal alignment score
     return D[n][m];
 }
+string backtrackingWF(const string& S1, const string& S2) {
+    string seq1_align = "";
+    string seq2_align = "";
+    int n = S1.size();
+    int m = S2.size();
+    int wex = 1;
+    int wop = 1;
+    int wsub = 1;
+    vector<int> contenders;
+    int minCon = 0;
 
-void calculateReadLocation(string read,  vector<RowMinimizer*> &MinimizersListForCPU) {
+    vector<vector<int>> D(n + 1, vector<int>(m + 1, 0));
+    vector<vector<int>> M1(n + 1, vector<int>(m + 1, 0));
+    vector<vector<int>> M2(n + 1, vector<int>(m + 1, 0));
+
+    // Initialize matrices with appropriate values
+    for (int i = 1; i <= n; ++i) {
+        D[i][0] = i * wex;
+        M1[i][0] = i * wex;
+    }
+    for (int j = 1; j <= m; ++j) {
+        D[0][j] = j * wex;
+        M2[0][j] = j * wex;
+    }
+
+    // Fill in the matrices using dynamic programming
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            M1[i][j] = min((M1[i - 1][j] + wex), (D[i - 1][j] + wop + wex));
+            M2[i][j] = min(M2[i][j - 1] + wex, D[i][j - 1] + wop + wex);
+
+            if (S1[i - 1] == S2[j - 1]) {
+                D[i][j] = D[i - 1][j - 1];
+            } else {
+                D[i][j] = min({M1[i][j], M2[i][j], D[i - 1][j - 1] + wsub});
+            }
+        }
+    }
+    for (int i = 0; i < n; i ++) {
+        for(int j = 0; j < m; j++){
+            std::cout << D[i][j] << ", ";
+        }
+        std::cout << "\n";
+    }
+
+
+    int i = m, j = n;
+    while (i > 0 && j > 0 && D[i][j] > 0) {
+        contenders = {D[i - 1][j - 1], D[i - 1][j], D[i][j - 1]};
+        minCon = *(std::min_element(contenders.begin(), contenders.end()));
+
+        if (minCon == contenders[0]) {
+            if (S1[i - 1] != S2[j - 1]) {
+                seq1_align += S2[i - 1];
+                seq2_align += S2[j - 1];
+            } else {
+                seq1_align += S1[i - 1];
+                seq2_align += S2[j - 1];
+            }
+            i -= 1;
+            j -= 1;
+        } else if (minCon == contenders[1]) {
+            seq1_align += S1[i - 1];
+            seq2_align += "-";
+            i -= 1;
+        } else {
+            seq1_align += "-";
+            seq2_align += S2[j - 1];
+            j -= 1;
+        }
+
+    }
+    std::reverse(seq1_align.begin(), seq1_align.end());
+    std::reverse(seq2_align.begin(), seq2_align.end());
+
+    return seq1_align;
+}
+
+int calculateReadLocation(string read,  vector<RowMinimizer*> &MinimizersListForCPU, int eth) {
     vector<Kmer*> minimizers = createMinimizers(read);
     int EdScore = -1;
     int minEdScore = MinimizersListForCPU[0]->refSeq.size();
     int address = 0;
     map<Kmer*,int> mapKmerToAdressInReq;
+    string minSubRef;
 
     for (Kmer* minimizer : minimizers) {
         for (const auto& row : MinimizersListForCPU) {
             if (row->min.kmerSeq == minimizer->kmerSeq) {
-                EdScore = wagnerFischerAffineGap(read, row->refSeq, 1, 1, 1);
+                string subRef = row->refSeq.substr(N - K + minimizer->position - eth, N + 2*eth);
+                EdScore = wagnerFischerAffineGap(read, subRef, 1, 1, 1);
                 if(EdScore < minEdScore) {
-                    address = row->min.position;
+                    address = row->min.position; // location of minimizer in the genome
                     minEdScore = EdScore;
+                    minSubRef = subRef;
                 }
             }
         }
-        if (EdScore != -1) {
-            mapKmerToAdressInReq[minimizer] = address;
-        }
-        EdScore = -1;
     }
+    return address;
 }
 
 int main() {
-    createMinimizers("231032101233101");
-    wagnerFischerAffineGap("231", "231", 1, 1, 1);
-    vector<RowMinimizer*> MinimizersListForCPU;
-    Kmer min1 = Kmer(0,"123");
-    Kmer min2 = Kmer(6,"231");
-    Kmer min3 = Kmer(8,"023");
-    MinimizersListForCPU.push_back(new RowMinimizer(min1, "0231032221233101"));
-    MinimizersListForCPU.push_back(new RowMinimizer(min2, "222032101233101"));
-    MinimizersListForCPU.push_back(new RowMinimizer(min3, "231032101233221"));
+    //createMinimizers("10213230312320233");
+    int n = 150;
+    int m = 145;
+    int score = 0;
+    string read = "002011023132211001032232000111103133301300000301000300330213222323223223222101132300331102130131222002320021022020031010320011132202210202203210232023";
+    string sub  = "0010132110210202021323222330101000222230210222330003323313212010313033321132023001112232112221201233112123221101300311330222111030030102001000232";
+    //vector<vector<int>> D(n + 1, vector<int>(m + 1, 0));
+    wagnerFischerAffineGap2(read, sub, &score, false, 1, 1, 1);
+    //backtrackingWF("AGGCCTA", "TAGCTTA");
+    //vector<RowMinimizer*> MinimizersListForCPU;
+    //Kmer min1 = Kmer(0,"123");
+    //Kmer min2 = Kmer(6,"231");
+    //Kmer min3 = Kmer(8,"023");
+    //MinimizersListForCPU.push_back(new RowMinimizer(min1, "0231032123233101"));
+    //MinimizersListForCPU.push_back(new RowMinimizer(min2, "222032123133101"));
+    //MinimizersListForCPU.push_back(new RowMinimizer(min3, "231032100323221"));
 
-    calculateReadLocation("231032101233101", MinimizersListForCPU);
+    //calculateReadLocation("231032101233101", MinimizersListForCPU, 2);
 
     return 0;
+}
+
+int wagnerFischerAffineGap2(const string& S1, const string& S2, int* score,  bool backtraching, int wop, int wex, int wsub) {
+    int n = S1.size();
+    int m = S2.size();
+    vector<int> contenders;
+    int minCon = 0;
+    string seq1_align = "";
+    string seq2_align = "";
+
+    // Initialize matrices D, M1, and M2 with appropriate dimensions
+    vector<vector<int>> D(n + 1, vector<int>(m + 1, 0));
+    vector<vector<int>> M1(n + 1, vector<int>(m + 1, 0));
+    vector<vector<int>> M2(n + 1, vector<int>(m + 1, 0));
+
+    // Initialize matrices with appropriate values
+    for (int i = 1; i <= n; ++i) {
+        D[i][0] = i * wex;
+        M1[i][0] = i * wex;
+    }
+    for (int j = 1; j <= m; ++j) {
+        D[0][j] = j * wex;
+        M2[0][j] = j * wex;
+    }
+
+    // Fill in the matrices using dynamic programming
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            M1[i][j] = min(M1[i - 1][j] + wex, D[i - 1][j] + wop + wex);
+            M2[i][j] = min(M2[i][j - 1] + wex, D[i][j - 1] + wop + wex);
+
+            if (S1[i - 1] == S2[j - 1]) {
+                D[i][j] = D[i - 1][j - 1];
+            } else {
+                D[i][j] = min({M1[i][j], M2[i][j], D[i - 1][j - 1] + wsub});
+            }
+        }
+    }
+    // Return the optimal alignment score
+    *score = D[n][m];
+
+    // find the alignment of the read according to sub reference sequence
+    if(backtraching) {
+        int i = n, j = m;
+        while (i > 0 && j > 0 && D[i][j] > 0) {
+            contenders = {D[i - 1][j - 1], D[i - 1][j], D[i][j - 1]};
+            minCon = *(std::min_element(contenders.begin(), contenders.end()));
+
+            if (minCon == contenders[0]) {
+                if (S1[i - 1] != S2[j - 1]) {
+                    seq1_align += S2[i - 1];
+                    seq2_align += S2[j - 1];
+                } else {
+                    seq1_align += S1[i - 1];
+                    seq2_align += S2[j - 1];
+                }
+                i -= 1;
+                j -= 1;
+            } else if (minCon == contenders[1]) {
+                seq1_align += S1[i - 1];
+                seq2_align += "-";
+                i -= 1;
+            } else {
+                seq1_align += "-";
+                seq2_align += S2[j - 1];
+                j -= 1;
+            }
+
+        }
+
+        std::reverse(seq1_align.begin(), seq1_align.end());
+        std::reverse(seq2_align.begin(), seq2_align.end());
+    }
+
+    return *score;
 }
