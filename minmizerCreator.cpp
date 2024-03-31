fe5c2f09df2b5b44eb50a87044b24efba49a7a0f
idanmagram <idan.magram@gmail.com>
2024-03-31 12:33:33 +0300

    100 iterations opt with BT

--- minmizerCreator.cpp
+++ minmizerCreator.cpp
@@ -20,7 +20,7 @@ int main() {
     int i = 0;
     int res = 0;
     for (i = 0; i < 100; i++) {
-        res = wagnerFischerAffineGap2(read, sub, &score, false, 1, 1, 1);
+        res = wagnerFischerAffineGap2(read, sub, &score, true, 1, 1, 1);
     }
     return res;
 }
