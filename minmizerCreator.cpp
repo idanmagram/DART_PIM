a80f82bfabf934080f0c1bf05ca28eadd74abc1d
idanmagram <idan.magram@gmail.com>
2024-03-31 12:30:02 +0300

    100 iterations opt with BT

--- minmizerCreator.cpp
+++ minmizerCreator.cpp
@@ -18,7 +18,7 @@ int main() {
     string sub  = "002011023132211001032232000111103133301300000301000300330213222323223223222101132300331102130131222002320021022020031010320011132202210202203210232023020220";
     int i = 0;
     int res = 0;
-    for (i = 0; i < 10000; i++) {
+    for (i = 0; i < 100; i++) {
         res = wagnerFischerAffineGap2(read, sub, &score, false, 1, 1, 1);
     }
     return res;
@@ -27,10 +27,10 @@ int main() {
 int wagnerFischerAffineGap2(const string& S1, const string& S2, int* score,  bool backtraching, int wop, int wex, int wsub) {
     const int n = S1.size();
     const int m = S2.size();
-    //vector<int> contenders;
-    //int minCon = 0;
-    //string seq1_align = "";
-    //string seq2_align = "";
+    vector<int> contenders;
+    int minCon = 0;
+    string seq1_align = "";
+    string seq2_align = "";
 
     int D[REF_SUB_SEQUENCE_LENGTH+1][READ_LENGTH+1] = {0};
     int M1[REF_SUB_SEQUENCE_LENGTH+1][READ_LENGTH+1] = {0};
@@ -62,6 +62,38 @@ int wagnerFischerAffineGap2(const string& S1, const string& S2, int* score,  boo
         }
     }
     *score = D[n][m];
+
+    if(backtraching) {
+        int i = n, j = m;
+        while (i > 0 && j > 0 && D[i][j] > 0) {
+            contenders = {D[i - 1][j - 1], D[i - 1][j], D[i][j - 1]};
+            minCon = *(std::min_element(contenders.begin(), contenders.end()));
+
+            if (minCon == contenders[0]) {
+                if (S1[i - 1] != S2[j - 1]) {
+                    seq1_align += S2[i - 1];
+                    seq2_align += S2[j - 1];
+                } else {
+                    seq1_align += S1[i - 1];
+                    seq2_align += S2[j - 1];
+                }
+                i -= 1;
+                j -= 1;
+            } else if (minCon == contenders[1]) {
+                seq1_align += S1[i - 1];
+                seq2_align += "-";
+                i -= 1;
+            } else {
+                seq1_align += "-";
+                seq2_align += S2[j - 1];
+                j -= 1;
+            }
+
+        }
+
+        std::reverse(seq1_align.begin(), seq1_align.end());
+        std::reverse(seq2_align.begin(), seq2_align.end());
+    }
     return *score;
 }
 
