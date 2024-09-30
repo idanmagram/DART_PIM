9632a9149f259b6ef3a3f144992062ac0e559e27
idanmagram <idan.magram@gmail.com>
2024-09-30 18:12:12 +0300

    WF initialization update

--- minmizerCreator.cpp
+++ minmizerCreator.cpp
@@ -27,10 +27,11 @@ int main(int argc, char* argv[]) {
     string sub  = "002301300302032130100103230320011332000010300300032000130231223103000030313103033030320330103310303220032311020010222002010223001003010302033313300221322022";
     int i = 0;
     int res = 0;
-    for (i = 0; i < 1000; i++) {
+    for (i = 0; i < 100; i++) {
         res = wagnerFischerAffineGap2(read, sub, &score, false, 1, 1, 1);
+        cout << score;
     }
-    return res;
+    return 0;
 
 /*
     ifstream readsFile;
@@ -62,7 +63,7 @@ void getReadsFromFile(ifstream& readsFile){
 }
 */
 
-int wagnerFischerAffineGap2(const string& S1, const string& S2, int* score,  bool backtraching, int wop, int wex, int wsub) {
+int wagnerFischerAffineGap2(const string& S1, const string& S2, int* score, bool backtracing, int wop, int wex, int wsub) {
     const int n = S1.size();
     const int m = S2.size();
     vector<int> contenders;
@@ -77,61 +78,43 @@ int wagnerFischerAffineGap2(const string& S1, const string& S2, int* score,  boo
     const int max_gap = MAX_GAP;
     const int max_gap_penalty = max_gap + wop;
 
+    // Optimized initialization for both first row and first column
+    for (int i = 1; i <= max(n, m); ++i) {
+        if (i <= n) { // Initialize first column (gaps in S1 aligned to empty S2)
+            D[i][0] = wop + (i - 1) * wex; // Gap penalty for aligning S1 to empty S2
+            M1[i][0] = D[i][0];            // Same gap penalty for M1
+            M2[i][0] = max_gap_penalty;     // M2 irrelevant here (no gaps in S1)
+        }
+        if (i <= m) { // Initialize first row (gaps in S2 aligned to empty S1)
+            D[0][i] = wop + (i - 1) * wex; // Gap penalty for aligning S2 to empty S1
+            M1[0][i] = max_gap_penalty;     // M1 irrelevant here (no gaps in S2)
+            M2[0][i] = D[0][i];            // Same gap penalty for M2
+        }
+    }
+
     // Fill the DP tables using dynamic programming
     for (int i = 1; i <= n; ++i) {
         for (int j = 1; j <= m; ++j) {
             if (abs(i - j) <= max_gap) {
-                if (abs (i - 1 - j) > max_gap) {
+                if (abs(i - 1 - j) > max_gap) {
                     M1[i - 1][j] = max_gap_penalty;
                     D[i - 1][j] = max_gap_penalty;
                 }
-                if (abs (i - (j - 1)) > max_gap) {
+                if (abs(i - (j - 1)) > max_gap) {
                     M2[i][j - 1] = max_gap_penalty;
                     D[i][j - 1] = max_gap_penalty;
                 }
 
                 M1[i][j] = min(M1[i - 1][j] + wex, D[i - 1][j] + wop + wex);
                 M2[i][j] = min(M2[i][j - 1] + wex, D[i][j - 1] + wop + wex);
+
                 if (S1[i - 1] == S2[j - 1])
                     D[i][j] = D[i - 1][j - 1];
                 else
-                    D[i][j] = min({(M1[i][j]), M2[i][j], D[i - 1][j - 1] + wsub});
+                    D[i][j] = min({M1[i][j], M2[i][j], D[i - 1][j - 1] + wsub});
             }
         }
     }
-    *score = D[n][m];
 
-    if(backtraching) {
-        int i = n, j = m;
-        while (i > 0 && j > 0) {
-            contenders = {D[i - 1][j - 1], D[i - 1][j], D[i][j - 1]};
-            minCon = *(std::min_element(contenders.begin(), contenders.end()));
-
-            if (minCon == contenders[0]) {
-                if (S1[i - 1] != S2[j - 1]) {
-                    seq1_align += S2[i - 1];
-                    seq2_align += S2[j - 1];
-                } else {
-                    seq1_align += S1[i - 1];
-                    seq2_align += S2[j - 1];
-                }
-                i -= 1;
-                j -= 1;
-            } else if (minCon == contenders[1]) {
-                seq1_align += S1[i - 1];
-                seq2_align += "-";
-                i -= 1;
-            } else {
-                seq1_align += "-";
-                seq2_align += S2[j - 1];
-                j -= 1;
-            }
-
-        }
-
-        std::reverse(seq1_align.begin(), seq1_align.end());
-        std::reverse(seq2_align.begin(), seq2_align.end());
-    }
-    return *score;
+    *score = D[n][m]; // Final alignment score
 }
-
